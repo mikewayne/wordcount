739aec80fe468bd1bc588511ba3d2f4a542f90ee
Michael Hoeft <student@epicodus.com>
2015-04-24 16:55:46 -0700

    A few more changes, still the same error

--- spec/wordcount_spec.rb
+++ spec/wordcount_spec.rb
@@ -2,14 +2,15 @@ require('rspec')
 require('./lib/wordcount.rb')
 
 describe('String#wordcount') do
-  it("takes an inputted string, outputs the first word in the string") do
+  it("takes an inputted string, outputs a '1' if there are '1' total of the first word in the string") do
     expect(("there everywhere here").wordcount()).to(eq(1))
-end
+  end
 end
 
-#describe('String@wordcount') do
-#  it("") do
-#    expect("")
+#describe('String#wordcount') do
+#  it("takes an inputted string, outputs a '2' if there are '2' total of the second word in the string") do
+#    expect(("We are are").wordcount()).to(eq(2))
 #  end
+#end
 
 #describe('String#wordcount.')
